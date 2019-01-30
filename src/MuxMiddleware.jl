53e38611885a310a0f7d813a156ca023a616eaac
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2019-01-30 15:04:21 -0600

    Initial commit

---
+++ src/MuxMiddleware.jl
@@ -0,0 +1,34 @@
+module MuxMiddleware
+
+using Mux
+
+export hostbranch, regexbranch
+
+"""
+    hostbranch(host::AbstractString, app...)
+
+Branches to `app...` when the request's hostname matches `host` exactly.
+"""
+hostbranch(host::AbstractString, app...) = branch(req -> begin
+  req_host_all = first(filter(hdr->hdr[1]=="Host", req[:headers]))[2]
+  # FIXME: Strip protocol
+  req_host = first(split(req_host_all, ":"))
+  return host == req_host
+end, app...)
+
+"""
+    regexbranch(regex::Regex, app...)
+
+Branches to `app...` when `regex` matches the request path. The path will
+always start with a '/'. If the regex matches, the RegexMatch result will be
+stored in the request's `:regex_match` field.
+"""
+regexbranch(regex::Regex, app...) = branch(req -> begin
+  matches = match(regex, "/" * join(req[:path], "/"))
+  matches === nothing && return false
+  req[:regex_match] = matches
+  return true
+end, app...)
+
+
+end # module
